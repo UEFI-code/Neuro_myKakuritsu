69b327023fdba16325a6753bfe5ca7bbc7e06e60
UEFI <1092700291@qq.com>
2022-08-28 04:21:47 -0400

    Update Code

--- Demo_myKakuritsu_Linear.py
+++ Demo_myKakuritsu_Linear.py
@@ -39,44 +39,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
-import myKakuritsu_Linear
-
-class myKakuritsu_Linear_Function(torch.autograd.Function):
-    # Note that both forward and backward are @staticmethods
-    @staticmethod
-    def forward(ctx, input, weight, Kakuritsu):
-        ctx.save_for_backward(input, weight)
-        #output = input.mm(weight.t())
-        #output = mylinear_cpp.forward(input, weight)
-        output = myKakuritsu_Linear.forward(input, weight, Kakuritsu)
-
-        return output[0]
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        input, weight = ctx.saved_tensors
-        #print(grad_output)
-        #grad_input = grad_weight = None
-        #grad_input = grad_output.mm(weight)
-        #grad_weight = grad_output.t().mm(input)
-        #grad_input, grad_weight = mylinear_cpp.backward(grad_output, input, weight)
-        grad_input, grad_weight = myKakuritsu_Linear.backward(grad_output, input, weight)
-
-        #print(grad_input)
-
-        return grad_input, grad_weight, grad_weight
-
-class myKakuritsu_Linear_Obj(nn.Module):
-    def __init__(self, input_features, output_features):
-        super(myKakuritsu_Linear_Obj, self).__init__()
-        self.input_features = input_features
-        self.output_features = output_features
-        self.weight = nn.Parameter(torch.Tensor(output_features, input_features))
-        self.weight.data.uniform_(-0.1, 0.1)
-        self.Kakuritsu = nn.Parameter(torch.ones(output_features, input_features) * 0.8)
-
-    def forward(self, input):
-        return myKakuritsu_Linear_Function.apply(input, self.weight, self.Kakuritsu)
+from myKakuritsu_Drv import myKakuritsu_Linear_Obj as myKakuritsu_Linear_Obj
 
 class Net(nn.Module):
     def __init__(self):
