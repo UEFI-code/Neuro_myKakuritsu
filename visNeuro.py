637b28f2e7903c075048bb968e83528c06f9096b
UEFI <1092700291@qq.com>
2022-10-07 13:39:16 +0800

    Bug Fix

--- visNeuro.py
+++ visNeuro.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import random
 
-def draw(x = [0.1, 0.2, 0.3, 0.1, 0.15], row = 2, scaleR = 50):
+def draw(x = [0.1, 0.2, 0.3, 0.1, 0.15], row = 2, scaleR = 50, title = 'out'):
 	#tmp = np.zeros((1024,1024,3))
 	# Caclulate the Resolution
 	tmp = np.zeros((row*scaleR*2, row*scaleR*2, 3))
@@ -24,12 +24,12 @@ def draw(x = [0.1, 0.2, 0.3, 0.1, 0.15], row = 2, scaleR = 50):
 		nowX = 0
 		maxR = 0
 
-	cv2.imwrite("out.jpg", tmp)
-	cv2.imshow("out", tmp)
+	cv2.imwrite(title + '.bmp', tmp)
+	cv2.imshow(title, tmp)
 	cv2.waitKey(0)
 
 if __name__ == "__main__":
 	paint = []
 	for i in range(64):
 		paint.append(random.random())
-	draw(paint, 8, 50)
+	draw(paint, 8, 50, 'try')
